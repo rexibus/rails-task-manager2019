4c931b2cf11a185e060bfee490db9bc38d9b60ea
RegisWeill <regis_weill@hotmail.com>
2019-05-09 01:09:44 +0200

    bootstrap, font awesome, index completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #list all the tasks
+  get "tasks", to: "tasks#index"
+  #show a specific tasks
+
+  #create a task
+
+  #update a task
+
+  #delete a task
+
+
+
 end
